2a37718f921dc122c23f05cf242f20b3e912e83d
Christopher Stojanovski <christopher.stojanovski@gmail.com>
2021-03-13 19:10:29 -0500

    E-mail upon failure to quickly restart program

--- rl_scraper.py
+++ rl_scraper.py
@@ -1,13 +1,41 @@
 # Unofficial package providing Python API for coinbase
 # https://github.com/danpaquin/coinbasepro-python
 import cbpro
+
 import time
 import math
 import pandas as pd
 import os
 
+# For config file
+import yaml
 
-def main():
+# For email sending
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+
+
+def email_notification():
+    with open('config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+
+    msg = MIMEMultipart()
+    msg['From'] = config['email']
+    msg['To'] = config['to']
+    msg['Subject'] = 'DISCONNECT DETECTED'
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(config['email'], config['password'])
+        server.sendmail(config['email'], config['to'], msg.as_string())
+
+
+def scraper(security='BTC-USD', request_time=10, dump_time=3600, send_email=True):
     public_client = cbpro.PublicClient()
 
     # Get the current working directory for print statement
@@ -15,29 +43,38 @@ def main():
 
     historical_ob = pd.DataFrame(columns=['Time', 'Bids', 'Asks'])
 
-    # ToDo: add try catch incase of disconnect send email
     while True:
-        cur_time = math.floor(time.time())
-        if cur_time % 10 == 0:
-            # Get the order book at level_2 granularity - this is the top 50 bids and asks
-            ob = public_client.get_product_order_book('BTC-USD', 2)
+        try:
+            cur_time = math.floor(time.time())
+            if cur_time % request_time == 0:
+                # Get the order book at level_2 granularity - this is the top 50 bids and asks
+                ob = public_client.get_product_order_book(security, 2)
 
-            # Pandas allows for enlargement using loc
-            historical_ob.loc[len(historical_ob)] = [cur_time, ob['bids'], ob['asks']]
+                # Pandas allows for enlargement using loc
+                historical_ob.loc[len(historical_ob)] = [cur_time, ob['bids'], ob['asks']]
 
-            # Every hour collected data is flushed to a csv, cleared, and continued
-            if cur_time % 60 == 0:
-                file_name = str(cur_time) + '.csv'
-                historical_ob.to_csv(file_name, index=False)
-                # ToDo: Save to Google Drive
-                print('File:', cur_time, 'saved in', path)
-                historical_ob.drop(historical_ob.index, inplace=True)
+                # Every hour collected data is flushed to a csv, cleared, and continued
+                if cur_time % dump_time == 0:
+                    file_name = str(cur_time) + '.csv'
+                    historical_ob.to_csv(file_name, index=False)
+                    # ToDo: Save to Google Drive
+                    print('File:', cur_time, 'saved in', path)
+                    historical_ob.drop(historical_ob.index, inplace=True)
 
-            # Ensures only 1 call every 10 seconds - additionally that calls are evenly
-            # spaced even when flushing every hour which will take substantially longer
-            # running under assumption that a flush is less than 9 seconds plus overhead
-            # of first call
-            time.sleep(1)
+                # Ensures only 1 call every 10 seconds - additionally that calls are evenly
+                # spaced even when flushing every hour which will take substantially longer
+                # running under assumption that a flush is less than 9 seconds plus overhead
+                # of first call
+                time.sleep(1)
+        except:
+            if send_email:
+                print('Error hit - sending email to restart')
+                email_notification()
+                return
+
+
+def main():
+    scraper()
 
 
 if __name__ == '__main__':
