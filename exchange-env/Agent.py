a5b88adbb7223e1b1a374101c73309cca639cb81
Christopher Stojanovski <christopher.stojanovski@gmail.com>
2021-04-22 23:00:43 -0400

    Reformatting agent to make it more readable

--- exchange-env/Agent.py
+++ exchange-env/Agent.py
@@ -1,16 +1,15 @@
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
 from stable_baselines import TRPO
 
 import ExchangeEnv
+import csv
 
-# When using SubprocVecEnv, users must wrap the code in an if __name__ == "__main__"
-if __name__ == '__main__':
+
+def train(train_data):
     # The algorithms require a vectorized environment to run
-    env_train = SubprocVecEnv([lambda: ExchangeEnv.ExchangeEnv(r"C:\Users\chris\OneDrive\Documents\GitHub\Data\Cumulative\Train",
-                                                               10000, 0)])
+    env_train = SubprocVecEnv([lambda: ExchangeEnv.ExchangeEnv(train_data, 10000, 0)])
 
     # In the paper a policy with a feed forward network with two hidden layers each consisting of 64 neurons was used
     policy_kwargs = dict(net_arch=[64, 64])
@@ -29,14 +28,39 @@ if __name__ == '__main__':
     model.learn(total_timesteps=10000000)
     model.save('ppo2_trader')
 
+
+def test(test_data, model_location):
     # Using a different environment to test the model
-    env_test = SubprocVecEnv([lambda: ExchangeEnv.ExchangeEnv(r"C:\Users\chris\OneDrive\Documents\GitHub\Data\Cumulative\Test",
-                                                              10000, 0)])
+    env_test = SubprocVecEnv([lambda: ExchangeEnv.ExchangeEnv(test_data, 10000, 0)])
+    model = PPO2.load(model_location)
     obs = env_test.reset()
     done = False
 
+    price_history = []
+    portfolio_value = []
+
     while not done:
         action, _states = model.predict(obs)
-        obs, rewards, done, info = env_test.step(action)
-        print(action)
-        # print(env_test.env_method("historical"))
+        obs, rewards, done, _ = env_test.step(action)
+
+        # Appending the current time steps highest bid
+        price_history.append(obs[0][0][0])
+
+        # Appending current portfolio value
+        portfolio_value.append(rewards)
+
+    with open("price_history.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerow(price_history)
+
+    with open("portfolio_value.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerow(portfolio_value)
+
+
+# When using SubprocVecEnv, users must wrap the code in an if __name__ == "__main__"
+if __name__ == '__main__':
+    # train(r"C:\Users\chris\OneDrive\Documents\GitHub\Data\Cumulative\Train")
+    test(r"C:\Users\chris\OneDrive\Documents\GitHub\Data\Cumulative\Test",
+         r"C:\Users\chris\OneDrive\Documents\GitHub\Plutus\exchange-env\ppo2_trader.zip")
+
